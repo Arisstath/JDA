d9fccd94327accbb620b6a5a39dbcb24afae3ad4
Florian Spieß <business@minnced.club>
2017-08-18 19:43:32 +0200

    Added Guild#getDefaultChannel to know which channel will open on joining for the first time

--- src/main/java/net/dv8tion/jda/core/entities/impl/GuildImpl.java
+++ src/main/java/net/dv8tion/jda/core/entities/impl/GuildImpl.java
@@ -42,6 +42,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import javax.annotation.Nullable;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
@@ -469,6 +470,17 @@ public class GuildImpl implements Guild
     public TextChannel getPublicChannel()
     {
         return textChannels.get(id);
+    }
+
+    @Nullable
+    @Override
+    public TextChannel getDefaultChannel()
+    {
+        final Role role = getPublicRole();
+        return getTextChannelsMap().valueCollection().stream()
+                .sorted(Comparator.reverseOrder())
+                .filter(c -> role.hasPermission(c, Permission.MESSAGE_READ))
+                .findFirst().orElse(null);
     }
 
     @Override
